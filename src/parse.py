6d10d915ebf9ac11e7f2354f87c0389bbcfbc5a5
Piotr Zieliński <piotrekzie100@gmail.com>
2023-10-25 18:17:13 +0200

    Add parsing MOT15 ground truth

--- src/parse.py
+++ src/parse.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterable
 from configparser import ConfigParser
 from pathlib import Path
 from typing import Any
@@ -18,3 +19,31 @@ def parse_mot15_ini(in_file: Path) -> dict[str, Any]:
         "height": int(parsed["imHeight"]),
         "extension": parsed["imExt"],
     }
+
+
+def parse_mot15_gt(in_file: Path, length: int) -> Iterable[dict[str, list[Any]]]:
+    """Parse MOT15 gt.txt file to a list of dicts with bboxes, classes and ids.
+
+    .. note: the default bbox format is [x1, y1, w, h]
+        we assume that the class is always 0
+    """
+    current_frame = 0
+    annotation: dict[str, list[Any]] = {"bboxes": [], "classes": [], "ids": []}
+    with open(in_file, "r") as fp:
+        for line in fp:
+            frame, obj, x1, y1, w, h, _, _, _, _ = line.split(",")
+            frame_id = int(frame) - 1
+            obj_id = int(obj) - 1
+            if int(frame_id) > current_frame:
+                for _ in range(frame_id - current_frame):
+                    yield annotation
+                    current_frame += 1
+                    annotation = {"bboxes": [], "classes": [], "ids": []}
+            annotation["bboxes"].append([int(x1), int(y1), int(w), int(h)])
+            annotation["classes"].append(0)
+            annotation["ids"].append(obj_id)
+
+    while current_frame < length:
+        yield annotation
+        annotation = {"bboxes": [], "classes": [], "ids": []}
+        current_frame += 1
--- tests/test_parse.py
+++ tests/test_parse.py
@@ -3,7 +3,7 @@ from unittest.mock import mock_open, patch
 
 import pytest
 
-from src.parse import parse_mot15_ini
+from src.parse import parse_mot15_gt, parse_mot15_ini
 
 
 @pytest.fixture
@@ -16,6 +16,19 @@ seqLength=450
 imWidth=1920
 imHeight=1080
 imExt=.jpg
+"""
+
+
+@pytest.fixture
+def mot15_gt():
+    return """1,1,415,449,129,269,1,-1,-1,-1
+3,1,581,443,97,269,1,-1,-1,-1
+3,2,1347,415,153,377,1,-1,-1,-1
+3,3,1429,423,161,339,1,-1,-1,-1
+6,1,413,449,131,269,1,-1,-1,-1
+6,2,579,443,97,269,1,-1,-1,-1
+6,3,1353,413,155,379,1,-1,-1,-1
+6,4,1437,421,157,341,1,-1,-1,-1
 """
 
 
@@ -32,3 +45,41 @@ def test_parse_mot_15_ini(seq_info):
     assert parsed["width"] == 1920
     assert parsed["height"] == 1080
     assert parsed["extension"] == ".jpg"
+
+
+@pytest.mark.parametrize("length", [7, 9, 10])
+def test_parse_mot15_gt(length, mot15_gt):
+    """Test if MOT15 ground truth is parsed properly."""
+    with patch("builtins.open", mock_open(read_data=mot15_gt)):
+        parsed = list(parse_mot15_gt(Path("dummy/path"), length))
+
+    assert len(parsed) == length
+
+    assert parsed[0]["bboxes"] == [[415, 449, 129, 269]]
+    assert parsed[0]["classes"] == [0]
+    assert parsed[0]["ids"] == [0]
+
+    assert parsed[1]["bboxes"] == parsed[1]["classes"] == parsed[1]["ids"] == []
+
+    assert parsed[2]["bboxes"] == [
+        [581, 443, 97, 269],
+        [1347, 415, 153, 377],
+        [1429, 423, 161, 339],
+    ]
+    assert parsed[2]["classes"] == [0, 0, 0]
+    assert parsed[2]["ids"] == [0, 1, 2]
+
+    assert parsed[3]["bboxes"] == parsed[3]["classes"] == parsed[3]["ids"] == []
+    assert parsed[4]["bboxes"] == parsed[4]["classes"] == parsed[4]["ids"] == []
+
+    assert parsed[5]["bboxes"] == [
+        [413, 449, 131, 269],
+        [579, 443, 97, 269],
+        [1353, 413, 155, 379],
+        [1437, 421, 157, 341],
+    ]
+    assert parsed[5]["classes"] == [0, 0, 0, 0]
+    assert parsed[5]["ids"] == [0, 1, 2, 3]
+
+    for i in range(6, length):
+        assert parsed[i]["bboxes"] == parsed[i]["classes"] == parsed[i]["ids"] == []
