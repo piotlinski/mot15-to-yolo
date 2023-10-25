1c509f648d82816c2ebc50b83ea22748727f1a7c
Piotr Zieliński <piotrekzie100@gmail.com>
2023-10-25 18:28:14 +0200

    Convert parsed annotations to yolo format

--- src/convert.py
+++ src/convert.py
@@ -1,6 +1,21 @@
+from collections.abc import Iterable
+from typing import Any
+
+
 def convert_to_yolo(bbox: list[int], width: int, height: int) -> list[float]:
     """Convert x1y1wh bbox to yolo format."""
     x1, y1, w, h = bbox
     x_center = (x1 + w / 2) / width
     y_center = (y1 + h / 2) / height
     return [x_center, y_center, w / width, h / height]
+
+
+def convert_parsed_to_yolo(
+    annotations: Iterable[dict[str, list[Any]]], width: int, height: int
+) -> Iterable[dict[str, list[Any]]]:
+    """Convert parsed annotations to yolo format."""
+    for annotation in annotations:
+        annotation["bboxes"] = [
+            convert_to_yolo(bbox, width, height) for bbox in annotation["bboxes"]
+        ]
+        yield annotation
--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.convert import convert_to_yolo
+from src.convert import convert_parsed_to_yolo, convert_to_yolo
 
 
 @pytest.mark.parametrize(
@@ -14,3 +14,21 @@ from src.convert import convert_to_yolo
 def test_convert_to_yolo(bbox, width, height, expected):
     """Test converting x1y1wh bbox to yolo format."""
     assert convert_to_yolo(bbox, width, height) == expected
+
+
+def test_convert_parsed_to_yolo():
+    """Test converting parsed annotations to yolo format."""
+    parsed = [
+        {"bboxes": [[10, 15, 3, 4], [0, 0, 20, 20]], "classes": [0, 0], "ids": [0, 1]},
+        {"bboxes": [], "classes": [], "ids": []},
+        {"bboxes": [[32, 76, 12, 4]], "classes": [0], "ids": [1]},
+    ]
+
+    converted = list(convert_parsed_to_yolo(parsed, 100, 100))
+
+    assert converted[0]["bboxes"] == [[0.115, 0.17, 0.03, 0.04], [0.1, 0.1, 0.2, 0.2]]
+    assert converted[1]["bboxes"] == []
+    assert converted[2]["bboxes"] == [[0.38, 0.78, 0.12, 0.04]]
+    for old, new in zip(parsed, converted, strict=True):
+        assert old["classes"] == new["classes"]
+        assert old["ids"] == new["ids"]
